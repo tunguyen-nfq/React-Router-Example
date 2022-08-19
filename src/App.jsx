064f04e3f83ae13d66dcecde60375028f61f979e
tunguyen-nfq <tu.nguyen@nfq.asia>
2022-08-19 10:02:15 +0700

    1 file modified, 3 deleted

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,6 @@ export default function App() {
   return (
     <div>
       <h1>Bookkeeper</h1>
-      <nav style={{ borderBottom: 'solid 1px', paddingBottom: '1rem' }}>
-        <Link to="/invoices">Invoices</Link> |{' '}
-        <Link to="/expenses">Expenses</Link>
-      </nav>
-      <Outlet />
     </div>
   );
 }
--- src/routes/expenses.jsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-export default function Expenses() {
-  return (
-    <main style={{ padding: '1rem 0' }}>
-      <h2>Expenses</h2>
-    </main>
-  );
-}
